c114b2fcbacbd74670809f0c293b94dfe21610f8
Juan Pablo Cárdenas <juan.cardenass@udea.edu.co>
2020-10-17 16:12:54 -0500

    Primer Commit: Se añade la primera estructura para un menu.

---
+++ parcial1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int opcion = -1;
+    while(opcion!=0)
+    {
+        cout <<"^^^^^^^^^^^^^ Programa de Defensa ^^^^^^^^^^^^^"<<endl;
+        cout <<"Ingrese 1: Generar disparos (al menos tres) ofensivos que comprometan la integridad del cañón defensivo."<<endl;
+        cout <<"Ingrese 2: Generar disparos (al menos tres) defensivos que comprometan la integridad del cañón ofensivo."<<endl;
+        cout <<"Ingrese 3: Dado un disparo ofensivo, generar un disparos defensivos que impida que el cañón defensivo sea destruido sin importar si el cañón ofensivo pueda ser destruido."endl;
+        cout <<"Ingrese 4: Dado un disparo ofensivo, generar disparo defensivos que impidan que los cañones defensivo y ofensivo puedan ser destruidos."<<endl;
+
+        switch(opcion)
+        {
+
+
+        }
+
+    }
+
+}
